7fcf47f1695f1914a8c53b43ebe980308fcc512a
Jeffery <jeffxie27@outlook.com>
2023-07-13 14:09:19 -0600

    Created template for recipes page.

---
+++ src/pages/recipes.tsx
@@ -0,0 +1,20 @@
+// Components
+import Navbar from "@/components/navbar";
+
+const Recipes = () => {
+
+
+  return (
+    <div className="bg-white h-screen flex items-center justify-center">
+      <Navbar></Navbar>
+
+      <input
+        className="bg-gray-50 rounded"
+        placeholder="Hi"
+      >
+      </input>
+    </div>
+  )
+}
+
+export default Recipes;
