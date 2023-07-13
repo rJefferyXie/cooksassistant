f9fc29f29de4da2bcb6fb1c93e34a4e38b270478
Jeffery <jeffxie27@outlook.com>
2023-07-13 14:09:10 -0600

    Created template for about page.

---
+++ src/pages/about.tsx
@@ -0,0 +1,17 @@
+// Components
+import Navbar from "@/components/navbar";
+
+const About = () => {
+  return (
+    <div className="bg-white h-screen flex items-center justify-center">
+      <Navbar></Navbar>
+
+      <p className="text-slate-800 font-bold">
+        Coming soon!
+
+      </p>
+    </div>
+  )
+}
+
+export default About;
