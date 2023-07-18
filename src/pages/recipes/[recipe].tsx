ba0c00f351be67eb7fca3f2dc2804f9fe7c555f6
Jeffery <jeffxie27@outlook.com>
2023-07-18 14:40:43 -0600

    Dynamic route with nextjs.

---
+++ src/pages/recipes/[recipe].tsx
@@ -0,0 +1,9 @@
+const Recipe = () => {
+  return (
+    <div className="w-screen h-screen z-50 bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      Hi
+    </div>
+  )
+}
+
+export default Recipe;
