72dabb8e9cbf25f72e3e3ad2ec3efc527a794e0a
Jeffery <jeffxie27@outlook.com>
2023-07-18 14:34:14 -0600

    Added spoonacular key.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     transpilePackages: ["next-image-export-optimizer"],
     env: {
+      NEXT_PUBLIC_SPOONACULAR_KEY: process.env.NEXT_PUBLIC_SPOONACULAR_KEY,
       nextImageExportOptimizer_imageFolderPath: "public/images",
       nextImageExportOptimizer_exportFolderPath: "out",
       nextImageExportOptimizer_quality: 75,
