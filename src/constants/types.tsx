25183ff6fa386234bc29c5b49dc7ba52779f63ba
Jeffery <jeffxie27@outlook.com>
2023-07-18 16:22:58 -0600

    Added types.

---
+++ src/constants/types.tsx
@@ -0,0 +1,18 @@
+// https://spoonacular.com/food-api/docs#Meal-Types
+
+const Types = [
+  "main course",
+  "side dish",
+  "dessert",
+  "appetizer",
+  "salad",
+  "bread",
+  "breakfast",
+  "soup",
+  "beverage",
+  "sauce",
+  "marinade",
+  "fingerfood",
+  "snack",
+  "drink"
+]
