551e5b899d599bf6bee7fbb417b07c4badb17072
Jeffery <jeffxie27@outlook.com>
2023-07-18 14:41:02 -0600

    Changed colors of background.

--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#80a2ff] to-[#89bffc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#80a2ff] to-[#89bffc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -33,18 +33,18 @@ const Hero = () => {
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               100+ Recipes From Around The World!
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-lg font-medium leading-8 text-gray-600">
               Welcome to Cook&apos;s Assistant, where you&apos;ll find a delightful collection of recipes to choose from. 
               Whether you&apos;re a seasoned chef or a cooking enthusiast, explore our diverse range of recipes and bring mouthwatering dishes to your table with ease.
             </p>
             <div className="mt-5 flex items-center justify-center gap-x-6">
               <a
                 onClick={() => navigate("recipes")}
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 cursor-pointer text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-sky-700 px-3.5 py-2.5 cursor-pointer text-sm font-semibold text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
               >
                 Get started!
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              <a className="text-sm cursor-pointer font-semibold leading-6 text-gray-900" onClick={() => navigate("about")}>
                 Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
@@ -55,7 +55,7 @@ const Hero = () => {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ffbd80] to-[#fc8989] opacity-40 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ffbd80] to-[#fc8989] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
