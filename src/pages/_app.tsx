26a027f9975e7d90872de575cb13ed2acbf606ce
Jeffery <jeffxie27@outlook.com>
2023-07-20 13:05:46 -0600

    Add redux.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css';
+// React + Next
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Components
+import Navbar from '@/components/navbar';
+
+// Tailwind
+import '@/styles/globals.css';
+
+// Redux
+import { PersistGate } from 'redux-persist/integration/react';
+import { wrapper, store, persistor } from "../store/store";
+import { Provider } from "react-redux";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navbar></Navbar>
+        <Component {...pageProps}/>
+      </PersistGate>
+    </Provider>
+  );
 }
+
+export default wrapper.withRedux(App);
