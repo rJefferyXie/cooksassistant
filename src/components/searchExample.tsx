5373ee641ec252bd5ff0715603df76887817ed5f
Jeffery <jeffxie27@outlook.com>
2023-07-21 16:51:05 -0600

    Search examples are now implemented.

--- src/components/searchExample.tsx
+++ src/components/searchExample.tsx
@@ -1,18 +1,49 @@
 // React + Next
 import ExportedImage from "next-image-export-optimizer";
 
+// Redux
+import { useDispatch } from "react-redux";
+import SearchActions from "@/store/actions/searchActions";
+
 interface SearchExampleProps {
   image: string,
-  search: Function
+  label: string,
+  search: Function,
+  query: string,
+  diets?: string[],
+  types?: string[],
+  cuisines?: string[],
+  intolerances?: string[]
 }
 
 const SearchExample = (props: React.PropsWithChildren<SearchExampleProps>) => {
-  const { image, search } = props;
+  const { image, label, search, query, diets, types, cuisines, intolerances } = props;
+
+  // Redux
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(SearchActions.setQuery(query));
+    dispatch(SearchActions.setDiets(diets || []));
+    dispatch(SearchActions.setTypes(types || []));
+    dispatch(SearchActions.setCuisines(cuisines || []));
+    dispatch(SearchActions.setIntolerances(intolerances || []));
+
+    const params = {
+      query: query,
+      diets: diets || [],
+      types: types || [],
+      cuisines: cuisines || [],
+      intolerances: intolerances || []
+    }
+
+    search(params);
+  }
 
   return (
     <button 
-      className="m-auto w-48 h-48 relative rounded-md hover:scale-105 hover:brightness-110 duration-300"
-      onClick={() => search()}
+      className="m-3 w-64 h-64 relative rounded-md hover:scale-105 hover:brightness-110 duration-300"
+      onClick={handleClick}
     >
       <ExportedImage 
         className="m-auto rounded-md w-64 h-64 cursor-pointer relative shadow-sm shadow-black"
@@ -23,8 +54,8 @@ const SearchExample = (props: React.PropsWithChildren<SearchExampleProps>) => {
       />
 
       <div className="text-slate-900 py-1 rounded-b-md w-full flex bg-zinc-700 bottom-0 absolute">
-        <p className="text-white text-center capitalize truncate w-5/6 m-auto">
-          Chicken Recipes
+        <p className="text-white text-center capitalize truncate w-full m-auto">
+          {label}
         </p>
       </div>                  
     </button>
