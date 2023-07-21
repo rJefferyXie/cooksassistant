8d4b80af1ea864a286be383ee8348ca40bf73d9d
Jeffery <jeffxie27@outlook.com>
2023-07-21 12:02:30 -0600

    Add actions and reducers for searching and recipes.

--- src/store/actions/allActions.ts
+++ src/store/actions/allActions.ts
@@ -1,7 +1,10 @@
 // Actions
+import RecipeActions from "./recipeActions";
+import SearchActions from "./searchActions";
 
 const allActions = {
-
+  RecipeActions,
+  SearchActions
 }
 
 export default allActions;
--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -2,9 +2,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 // Reducers
+import RecipeReducer from './recipeReducer';
+import SearchReducer from './searchReducer';
 
 const rootReducer = combineReducers({
-
+  recipe: RecipeReducer,
+  search: SearchReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
