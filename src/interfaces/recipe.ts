6fec2494fb438bdaff93aebd11ddc2b810b27a64
Jeffery <jeffxie27@outlook.com>
2023-07-31 14:20:34 -0600

    Add extra cooking information to recipe interface.

--- src/interfaces/recipe.ts
+++ src/interfaces/recipe.ts
@@ -1,8 +1,27 @@
+interface Instruction {
+  number: number,
+  step: string
+}
+
+interface Instructions {
+  name: string,
+  steps: Instruction[]
+}
+
 interface Recipe {
   id: number,
   title: string,
   image: string,
-  spoonacularSourceUrl: string
+  spoonacularSourceUrl: string,
+  readyInMinutes: number,
+  servings: number,
+  vegetarian: boolean,
+  vegan: boolean,
+  glutenFree: boolean,
+  dairyFree: boolean,
+  aggregatelikes: number,
+  healthScore: number,
+  analyzedInstructions: Instructions[]
 }
 
 export default Recipe;
